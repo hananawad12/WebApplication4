53c1ebd6dedf75f20326e17fd969dd33d3f7b0f2
Hanan awad <Hanan awad@DESKTOP-R4GA05B>
2019-06-01 14:36:45 -0700

    Controllers for post,like and comment

--- WebApplication4/Controllers/CommentController.cs
+++ WebApplication4/Controllers/CommentController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using WeddingGo.Models;
+using WeddingGo.Models.Repositery;
 
 namespace WeddingGo.Controllers
 {
@@ -11,5 +15,120 @@ namespace WeddingGo.Controllers
     [Route("api/Comment")]
     public class CommentController : ControllerBase
     {
+        private readonly IClientRepositery<Comment> db;
+        private readonly IConfiguration config;
+        public CommentController(IClientRepositery<Comment> _db, IConfiguration _config)
+        {
+            db = _db;
+            config = _config;
+        }
+
+        /// GRUD Operations
+
+        // GET: api/Comment
+        [HttpGet]
+        public IActionResult GetComments()
+        {
+            List<Comment> Comments = db.GetAll();
+
+            if (Comments.Count > 0)
+                return Ok(Comments);
+            else
+                return NotFound();
+        }
+
+        // GET: api/Comment/5
+        [HttpGet("{id}")]
+        public IActionResult GetComment([FromRoute] int id)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var Comment = db.GetById(id);
+
+            if (Comment == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Comment);
+        }
+
+        // PUT: api/Comment/5
+        [HttpPut("{id}")]
+        public IActionResult PutComment([FromRoute] int id, [FromBody] Comment Comment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != Comment.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Update(Comment);
+
+            try
+            {
+                db.Save();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!db.ItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(200);
+        }
+
+        // POST: api/Comment
+        [HttpPost]
+        public IActionResult PostComment([FromBody] Comment Comment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Insert(Comment);
+            db.Save();
+
+            return StatusCode(201);
+        }
+
+        // DELETE: api/Comment/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteComment([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var Comment = db.GetById(id);
+            if (Comment == null)
+            {
+                return NotFound();
+            }
+
+            db.Delete(id);
+            db.Save();
+
+            return Ok(Comment);
+
+        }
+
+
     }
 }
--- WebApplication4/Controllers/LikeController.cs
+++ WebApplication4/Controllers/LikeController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using WeddingGo.Models;
+using WeddingGo.Models.Repositery;
 
 namespace WeddingGo.Controllers
 {
@@ -11,5 +15,118 @@ namespace WeddingGo.Controllers
     [Route("api/Like")]
     public class LikeController : ControllerBase
     {
+        private readonly IClientRepositery<Like> db;
+        private readonly IConfiguration config;
+        public LikeController(IClientRepositery<Like> _db, IConfiguration _config)
+        {
+            db = _db;
+            config = _config;
+        }
+
+        /// GRUD Operations
+
+        // GET: api/Like
+        [HttpGet]
+        public IActionResult GetLikes()
+        {
+            List<Like> Likes = db.GetAll();
+
+            if (Likes.Count > 0)
+                return Ok(Likes);
+            else
+                return NotFound();
+        }
+
+        // GET: api/Like/5
+        [HttpGet("{id}")]
+        public IActionResult GetLike([FromRoute] int id)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var Like = db.GetById(id);
+
+            if (Like == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Like);
+        }
+
+        // PUT: api/Like/5
+        [HttpPut("{id}")]
+        public IActionResult PutLike([FromRoute] int id, [FromBody] Like Like)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != Like.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Update(Like);
+
+            try
+            {
+                db.Save();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!db.ItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(200);
+        }
+
+        // POST: api/Like
+        [HttpPost]
+        public IActionResult PostLike([FromBody] Like Like)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Insert(Like);
+            db.Save();
+
+            return StatusCode(201);
+        }
+
+        // DELETE: api/Like/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteLike([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var Like = db.GetById(id);
+            if (Like == null)
+            {
+                return NotFound();
+            }
+
+            db.Delete(id);
+            db.Save();
+
+            return Ok(Like);
+
+        }
     }
 }
